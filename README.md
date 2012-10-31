12a6e50ccc9bf82bc05a77794c2f0a306b4501b7
Derek Smith <dsmith@oversee.net>
2012-10-31 13:29:01 -0700

    Improved Documentation - Auto-Commit - Wed Oct 31 13:29:01 2012

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ perl ./Build.PL
 
 This will build, test and install the ```DeviceAtlas::API``` perl module.  This will require ```Module::Build```.
 
+This module supports perldoc: ```perldoc DeviceAtlas::API```
+
 Dependencies
 --------------
 Modules:
@@ -22,4 +24,76 @@ JSON::XS
 ```
 The use of this API requires a DeviceAtlas Enterprise JSON file that can be obtained with a license.  In order to properly test the API please place a copy of the JSON file in the test data directory as: ```t/data/donotadd.json```
 
+###Usage###
+```perl
+use DeviceAtlas::API;
+my $parser = DeviceAtlas::API->new('path_to_json_file');
+```
 Cheers!
+
+Module Description
+==================
+###MAIN METHODS###
+
+####new####
+
+  Initializes a new DeviceAtlas API object an calls ```_init```
+
+####getProperties####
+
+  Given an input User Agent string, parse and traverse the tree to gather all available properties for that User Agent. This is the main function that should be used. Calls ```trim```, ```seekProperties```, ```propertFromId```, ```valueAsTypedFromId```, ```valueFromId```.  This will return a hash refernce with ALL available properties for a given User Agent.
+
+####getTreeFromString####
+
+  Loads the JSON tree from file path.  Called by ```_init``` and results are stored in ```$self->{input}```. Loads tree into ```$self->{tree}```.
+
+####listProperties####
+
+  Returns a hash reference to s list of all available named properties in a tree.
+
+####idFromProperty####
+
+  Given a named property, returns the int 'id' for that property. If that property does not exists, then warn that it is not in the tree.
+
+####propertyFromId####
+
+  Given an int 'id', return the named property for that 'id'.
+
+####trim####
+
+  Removes leading whitespace and trailing whitespace + eol chars from string.
+
+####getProperty####
+
+  Given a sought property, return the value of that property if it is defined.
+
+####valueFromId####
+
+  Given an in 'id', return the value for that 'id' from the tree.
+
+####seekProperties####
+
+  Traverses the depths of the tree to determine the properties for a matched User Agent.
+
+####valueAsTypedFromId####
+
+  Returns a tree object pertaining to the given 'id'.
+
+  * Currently not functioning.
+
+###SUPPORT METHODS###
+
+####input####
+
+  Accessor for ```$self->{input}```
+
+####tree####
+
+  Accessor for ```$self->{tree}```
+
+###PRIVATE METHODS###
+
+####_init####
+
+  Called by ```new``` and calls ```getTreeFromString```
+
--- lib/DeviceAtlas/API.pm
+++ lib/DeviceAtlas/API.pm
@@ -5,7 +5,7 @@ package DeviceAtlas::API;
 
 use JSON;
 
-my $VERSION = '1.0';
+my $VERSION = '1.1';
 
 my $jcoder = JSON::XS->new->allow_nonref;
 
@@ -200,3 +200,118 @@ sub _init {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+  DeviceAtlas::API
+
+=head1 DESCRIPTION
+
+DeviceAtlas::API - DeviceAtlas Enterprise API for Perl
+
+Thank you very much to the initial work done by [darkonie](https://github.com/darkonie/Device-atlas-API-in-Perl "Maksym Naboka") for building a good starting point.
+
+This work was done for [Oversee.net](http://www.oversee.net/ "Oversee.net") with use of the [DeviceAtlas Enterprise Liscense](http://deviceatlas.com/resourcecentre/Get+Started/Enterprise+API).
+
+=head1 DEPENDENCIES
+
+Modules:
+Mobule::Build
+JSON::XS
+
+The use of this API requires a DeviceAtlas Enterprise JSON file that can be obtained with a license.  In order to properly test the API please place a copy of the JSON file in the test data directory as: t/data/donotadd.json
+
+Cheers!
+
+=head1 SYNOPSIS
+
+  use DeviceAtlas::API;
+
+  my $parser = DeviceAtlas::API->new('path_to_json_file');
+
+=head1 AUTHOR
+
+  Derek Smith (dsmith [at] oversee.net)
+
+=head1 MAIN METHODS
+
+=over 4
+
+=item B<new>
+
+  Initializes a new DeviceAtlas API object an calls "_init"
+
+=item B<getProperties>
+
+  Given an input User Agent string, parse and traverse the tree to gather all available properties for that User Agent. This is the main function that should be used. Calls 'trim', 'seekProperties', 'propertFromId', 'valueAsTypedFromId', 'valueFromId'.  This will return a hash refernce with ALL available properties for a given User Agent.
+
+=item B<getTreeFromString>
+
+  Loads the JSON tree from file path.  Called by _init and results are stored in $self->{input}. Loads tree into $self->{tree}.
+
+=item B<listProperties>
+
+  Returns a hash reference to s list of all available named properties in a tree.
+
+=item B<idFromProperty>
+
+  Given a named property, returns the int 'id' for that property. If that property does not exists, then warn that it is not in the tree.
+
+=item B<propertyFromId>
+
+  Given an int 'id', return the named property for that 'id'.
+
+=item B<trim>
+
+  Removes leading whitespace and trailing whitespace + eol chars from string.
+
+=item B<getProperty>
+
+  Given a sought property, return the value of that property if it is defined.
+
+=item B<valueFromId>
+
+  Given an in 'id', return the value for that 'id' from the tree.
+
+=item B<seekProperties>
+
+  Traverses the depths of the tree to determine the properties for a matched User Agent.
+
+=item B<valueAsTypedFromId>
+
+  Returns a tree object pertaining to the given 'id'.
+
+  ***Currently not functioning.
+
+=back
+
+=head1 SUPPORT METHODS
+
+=over 4
+
+=item B<input>
+
+  Accessor for $self->{input}
+
+=item B<tree>
+
+  Accessor for $self->{tree}
+
+=back
+
+=head1 PRIVATE METHODS
+
+=over 4
+
+=item B<_init>
+
+  Called by 'new' and calls 'getTreeFromString'
+
+=back
+
+=cut
+
